c243f1da373a0331c5cc8e5e595c9b6b50937191
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2022-11-23 14:13:06 -0500

    add html, css, and begining JS

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var start = document.getElementById("strt")
+
+start.addEventListener('click', begin)
+function begin() {
+console.log("started")
+}
+
+function newQuestion() {
+
+}
+
+function answer() {
+
+}
