761250bcc66c01e2638d5319dca0e8e289ba19ea
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2022-11-23 18:43:53 -0500

    add first function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
 var start = document.getElementById("strt")
+var questionSection = document.getElementById ("question-container")
 
 start.addEventListener('click', begin)
 function begin() {
 console.log("started")
+start.classList.add("hidden")
+questionSection.classList.remove("hidden")
 }
 
 function newQuestion() {
